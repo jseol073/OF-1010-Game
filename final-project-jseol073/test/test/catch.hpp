5f7f93de808af2f1f453a199016a3ad6369b793e
johnseol <kseol4@illinois.edu>
2018-05-01 19:47:26 -0500

    Added test cases when checking if rows or columns get updated when filled

---
+++ final-project-jseol073/test/test/catch.hpp
@@ -0,0 +1,13 @@
+//
+//  catch.hpp
+//  test
+//
+//  Created by John Seol on 5/1/18.
+//  Copyright © 2018 John Seol. All rights reserved.
+//
+
+#ifndef catch_h
+#define catch_h
+
+
+#endif /* catch_h */
